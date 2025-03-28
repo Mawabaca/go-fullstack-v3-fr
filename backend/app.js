8996f162cfef1204b10c2db3e0e9addabf1d933d
Abderahmane <abderahmane.benomari7@gmail.com>
2025-03-28 11:57:31 +0100

    add gitignore

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.json());
 
+console.log(process.env.DB_NAME)
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
