a2b2f5948569daca359aab4f396db514bb0e9de4
Abderahmane <abderahmane.benomari7@gmail.com>
2025-04-04 11:05:42 +0200

    Full

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,17 @@
 const express = require('express');
-const Thing = require('./models/thing');
+const app = express();
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGODB_URI;
 
 require('dotenv').config();
-
-const mongoose = require('mongoose');
-
-const mongoURI = process.env.MONGODB_URI;
 
 mongoose.connect(mongoURI)
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((err) => console.error('Connexion à MongoDB échouée !', err));
 
-
-const app = express();
-
 app.use(express.json());
-
-console.log('📦 Base utilisée :', process.env.DB_NAME);
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -31,39 +26,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
 
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-
-});
-
-app.use('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-
-});
 module.exports = app;
