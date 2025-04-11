26da82d3cd1a88c6fc77ceaf912abc571a9dad31
Abderahmane <abderahmane.benomari7@gmail.com>
2025-04-11 10:32:37 +0200

    sss

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
+const path = require('path');
 
 const mongoose = require('mongoose');
 
@@ -28,6 +29,7 @@ app.use((req, res, next) => {
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 module.exports = app;
